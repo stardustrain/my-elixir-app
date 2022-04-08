8f1294c60a8aca3a34cd76079749a17a404d2b9b
stardustrain <kthanterran@gmail.com>
2022-04-08 11:29:36 +0900

    test: add MyApp.Account.create_user/1 test cases

---
+++ test/my_app/account_test.exs
@@ -0,0 +1,19 @@
+defmodule MyApp.AccountTest do
+  use MyApp.DataCase
+
+  alias MyApp.Account
+  alias MyApp.Account.User
+
+  test "create_user/1 creates a user with valid data" do
+    valid_attrs = %{email: "test@test.com", password: "test1234"}
+    {:ok, %User{} = user} = Account.create_user(valid_attrs)
+
+    assert user.email === valid_attrs.email
+    assert user.password_hash !== nil
+  end
+
+  test "create_user/1 returns error with invalid data" do
+    assert {:error, %Ecto.Changeset{}} = Account.create_user()
+    assert {:error, %Ecto.Changeset{}} = Account.create_user(%{email: "test@test.com", password: "짧은암호"})
+  end
+end
