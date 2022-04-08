210fb41796afda07e7961d89a6b9a187ec8750e2
stardustrain <kthanterran@gmail.com>
2022-04-08 11:03:11 +0900

    feat: add Guardian, Pbkdf2 dependency

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule MyApp.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:guardian, "~> 2.0"},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
